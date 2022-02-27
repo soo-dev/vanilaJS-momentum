d1d802617dc180dd445b32cb18742797d83a2f79
soo <sooyoungs0120@gmail.com>
2022-02-27 22:25:58 +0900

    🧑‍💻 Add first basic login form EX

---
+++ app.js
@@ -0,0 +1,10 @@
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {
+    console.log(loginInput.value);
+    console.log("click");
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
+
